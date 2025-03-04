061b9a8329e2f800bf83196901d1cdb66de418ff
Andrew Jowe <andrewjowe@yahoo.com>
2025-03-03 16:37:49 -0800

    no eslint

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-import {useState} from "react";
+//import {useState} from "react";
 import Plane from "@/public/icons/Plane";
 import Image from "next/image";
 
 export default function Contact() {
-  const [name, setName] = useState("");
+  /*const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");*/
 
   return <form className="flex flex-col h-full">
     <div className="flex gap-6 pb-6">
       <div className="flex-1">
         <p className="font-bold pb-2">Your Name</p>
-        <input required className="text w-full" type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+        <input required className="text w-full" type="text" placeholder="Name"/>
       </div>
       <div className="flex-1">
         <p className="font-bold pb-2">Your Email</p>
-        <input required className="text w-full" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+        <input required className="text w-full" type="text" placeholder="Email"/>
       </div>
     </div>
     <p className="font-bold pb-2">Your Message</p>
-    <textarea required className="w-full flex-grow h-full mb-6 md:min-h-0 min-h-[200px]" placeholder="Message" onChange={(e) => setMessage(e.target.value)}/>
+    <textarea required className="w-full flex-grow h-full mb-6 md:min-h-0 min-h-[200px]" placeholder="Message"/>
     <div className="flex justify-between">
       <div>
         <button className="button blue">
